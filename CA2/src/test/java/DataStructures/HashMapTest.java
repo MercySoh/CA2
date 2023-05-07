b761c12acaebaee4ad3049fd0891a927c16d5f54
evanm <evanm@192.168.1.64>
2023-05-07 22:45:05 +0100

    Added HashMapTests

--- CA2/src/test/java/DataStructures/HashMapTest.java
+++ CA2/src/test/java/DataStructures/HashMapTest.java
@@ -25,12 +25,14 @@ public class HashMapTest {
         assertEquals("Doe", map.get("Jane"));
         assertEquals("Doe", map.get("Jonathan"));
     }
-    
+
     @Test
     public void testSize() throws SlotOccupiedException {
         map = new HashMap(2);
         assertEquals(0, map.size());
         map.put("John, Doe");
+        assertEquals(1, map.size());
         map.put("Jane, Doe");
+        assertEquals(2, map.size());
     }
 }
